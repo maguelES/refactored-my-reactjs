f0b831d2d70f4bd69cca8bdb3a11d3bb9b59446d
amsyar <nerokyt2@gmail.com>
2021-02-12 12:52:39 +0800

    Remove default design. Only includes trade list for now.

---
+++ src/features/trade/components/public/trade_public_list.component.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export function TradePublicList() {
+    return (
+        <div>
+            <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+                <div className="flex-shrink-0">
+                    <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo"/>
+                </div>
+                <div>
+                    <div className="text-xl font-medium text-black">ChitChat</div>
+                    <p className="text-gray-500">You have a new message!</p>
+                </div>
+            </div>
+        </div>
+    )
+}
