d678044d156275970f2ca8316205ca1a2526a952
Mohd Amsyar <nerokyt2@gmail.com>
2021-09-24 16:10:07 +0800

    Test display title

--- src/features/trade/components/public/trade_public_list.component.js
+++ src/features/trade/components/public/trade_public_list.component.js
@@ -1,6 +1,10 @@
 import React from "react";
+import {useSelector} from "react-redux";
+import {selectTradeListTitle} from "../../logic/store/tradeListStore";
 
 export function TradePublicList() {
+    const title = useSelector(selectTradeListTitle);
+
     return (
         <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
             <div className="md:flex">
@@ -10,7 +14,7 @@ export function TradePublicList() {
                          alt="Man looking at item at a store"/>
                 </div>
                 <div className="p-8 md:text-left">
-                    <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
+                    <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{title}</div>
                     <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding
                         customers for your new business</a>
                     <p className="mt-2 text-gray-500">Getting a new business off the ground is a lot of hard work.
