78fff3398ea7e21bb371930f5409c1be79cffbe0
Mohd Amsyar <nerokyt2@gmail.com>
2021-09-24 15:27:04 +0800

    Create tradeListStore.js

---
+++ src/features/trade/logic/store/tradeListStore.js
@@ -0,0 +1,13 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+export const tradeListStore = createSlice({
+    name: "tradeListStore",
+    initialState: {
+        title: "",
+        subtitle: "",
+        description: ""
+    },
+    reducers: {
+
+    }
+})
