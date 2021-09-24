a49eacce41706bfcc5be1842b7afeed2710e94fa
Mohd Amsyar <nerokyt2@gmail.com>
2021-09-24 16:17:12 +0800

    Test using sub and desc from store

--- src/features/trade/logic/store/tradeListStore.js
+++ src/features/trade/logic/store/tradeListStore.js
@@ -3,13 +3,15 @@ import {createSlice} from "@reduxjs/toolkit";
 export const tradeListStore = createSlice({
     name: "tradeList",
     initialState: {
-        title: "Case Study II",
-        subtitle: "",
-        description: ""
+        title: "Case Study IV",
+        subtitle: "Finding customers might not be a new business after all..",
+        description: "Getting business yadaa yadaa daa"
     },
     reducers: {}
 })
 
 export const selectTradeListTitle = state => state.tradeList.title;
+export const selectTradeListSubtitle = state => state.tradeList.subtitle;
+export const selectTradeListDescription = state => state.tradeList.description;
 
 export default tradeListStore.reducer;
