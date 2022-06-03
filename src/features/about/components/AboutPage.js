93c6a1656b1069ec7c903a60972a3a0a9228cff6
Mohd Amsyar <nerokyt2@gmail.com>
2022-06-03 09:59:32 +0800

    Test transistion

--- src/features/about/components/AboutPage.js
+++ src/features/about/components/AboutPage.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { Transition } from '@headlessui/react'
 
 export function AboutPage() {
 
     return (
-        <div className="flex flex-col flex-grow justify-items-start">
+        <Transition
+            as="div"
+            appear={true} //THIS will make the transition run everytime the component is rendered
+            show={true}
+            enter="transition-opacity duration-25"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity duration-150"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            className="flex flex-col flex-grow justify-items-start"
+        >
+
             <div className="mb-5 self-start flex-shrink-1">
                 <span className="text-3xl font-bold text-gray-900">
                 About Us
@@ -25,6 +38,6 @@ export function AboutPage() {
                     </div>
                 </div>
             </div>
-        </div>
+        </Transition>
     );
 }
