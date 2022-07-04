6c0703fc428467ce8df67ab134fc5c1c581443bd
Mohd Amsyar <nerokyt2@gmail.com>
2022-07-04 14:58:48 +0800

    Test trade list reducer

--- src/features/trade/components/market/MarketPage.js
+++ src/features/trade/components/market/MarketPage.js
@@ -1,24 +1,43 @@
-import React, { useState} from "react";
-import {useSelector} from "react-redux";
+import React from "react";
+import {useDispatch, useSelector} from "react-redux";
 import {
     selectTradeListDescription,
     selectTradeListSubtitle,
-    selectTradeListTitle
+    selectTradeListTitle,
+    tradeItemAdded
 } from "../../logic/store/tradeListSlice";
 import {MarketItemCard} from "../public/MarketItemCard";
 
 export function MarketPage() {
 
-    const [header, setHeader] = useState('')
-    const [subHeader, setSubHeader] = useState('')
+    const dispatch = useDispatch();
 
+    // Redux State
     const title = useSelector(selectTradeListTitle);
     const subtitle = useSelector(selectTradeListSubtitle)
     const desc = useSelector(selectTradeListDescription);
 
+    const onAddItemClicked = () => {
+
+        dispatch(
+            tradeItemAdded({
+                header: "Test Test Test",
+                subHeader: "Test Omega Test Test. Houston, are you there?",
+                description: "Yes, I am here."
+            })
+        );
+    }
+
     return (
         <div className="flex flex-col flex-auto">
             <div className={"self-start"}><h1 className="text-3xl font-bold text-gray-900 mb-5">Market</h1></div>
+            <section>
+                <form>
+                    <button type="button" onClick={onAddItemClicked}>
+                        Save Post
+                    </button>
+                </form>
+            </section>
             <div className="flex">
                 <div className="grid lg:grid grid-cols-2 grid-rows-2 items-center items-center gap-4">
                     <MarketItemCard title={title} subtitle={subtitle} desc={desc}/>
--- src/features/trade/logic/store/tradeListSlice.js
+++ src/features/trade/logic/store/tradeListSlice.js
@@ -7,11 +7,19 @@ export const tradeListSlice = createSlice({
         subtitle: "Finding customers might not be a new business after all..",
         description: "Getting business yadaa yadaa daa"
     },
-    reducers: {}
+    reducers: {
+        tradeItemAdded(state, action) {
+            state.title = action.payload.header;
+            state.subtitle = action.payload.subHeader;
+            state.description = action.payload.description;
+        }
+    }
 })
 
 export const selectTradeListTitle = state => state.tradeList.title;
 export const selectTradeListSubtitle = state => state.tradeList.subtitle;
 export const selectTradeListDescription = state => state.tradeList.description;
+
+export const { tradeItemAdded } = tradeListSlice.actions
 
 export default tradeListSlice.reducer;
