43913b50a686bd690f67801fe050bf652dda614d
Mohd Amsyar <nerokyt2@gmail.com>
2022-07-05 15:13:04 +0800

    Update MarketItemCard.js

--- src/features/trade/components/public/MarketItemCard.js
+++ src/features/trade/components/public/MarketItemCard.js
@@ -1,12 +1,18 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import {DotsHorizontalIcon, TrashIcon} from "@heroicons/react/outline";
+import {TruckIcon} from "@heroicons/react/solid";
 
 export function MarketItemCard({desc, subtitle, title}) {
     return (
         <div className="max-w-md bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mb-5">
             <div className="md:flex-col">
                 <div className={"flex mx-4 my-4"}>
+
+                    <div className={"flex rounded-full bg-gray-100 p-2"}>
+                        <TruckIcon className={"h-7 w-7 text-pink-600"}></TruckIcon>
+                    </div>
+
                     <div className={"flex gap-1 ml-auto"}>
                         <button type={"button"} className={"text-gray-500 hover:bg-gray-200 p-2 rounded-lg"}>
                             <TrashIcon className={"h-5 w-5 "}/>
@@ -17,9 +23,8 @@ export function MarketItemCard({desc, subtitle, title}) {
                     </div>
                 </div>
                 <div className="px-8 py-6 md:text-left">
-                    <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{title}</div>
-                    <a href="#"
-                       className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">{subtitle}</a>
+                    <div className="uppercase tracking-wide text-lg text-gray-800-500 font-bold">{title}</div>
+                    <p className="mt-1 leading-tight text-gray-500">{subtitle}</p>
                     <p className="mt-2 text-gray-500">{desc}</p>
                 </div>
             </div>
