cf189cc435594678d9f1629cf270de19698b44e6
Mohd Amsyar <nerokyt2@gmail.com>
2022-07-15 12:24:46 +0800

    col on mobile, row on md

--- src/features/personal/components/PersonalPage.js
+++ src/features/personal/components/PersonalPage.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import {PersonalPageTab} from "./PersonalPageTab";
+import {PersonalPageProfile} from "./PersonalPageProfile";
 
 export function PersonalPage() {
+
+    const [page] = useState("profile");
+    console.log(page);
+
     return (
         <div className="flex flex-col flex-grow justify-items-start">
             <div className="mb-5 self-start flex-shrink-1">
@@ -10,11 +15,12 @@ export function PersonalPage() {
                 </span>
             </div>
 
-            <div className={"flex gap-10"}>
+            <div className={"flex flex-col md:flex-row gap-10"}>
                 <PersonalPageTab/>
 
                 <div className="flex bg-white shadow px-5 py-4 rounded-md mt-2 ml-3 flex-grow">
-                    <span className="text-lg font-bold text-gray-800">User&apos;s Name</span>
+                    {/*<span className="text-lg font-bold text-gray-800">User&apos;s Name</span>*/}
+                    <PersonalPageProfile/>
                 </div>
             </div>
         </div>
