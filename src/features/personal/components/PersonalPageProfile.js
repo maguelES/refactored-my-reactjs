436ee67278cec76984dc5077ae26d80314a062ef
Mohd Amsyar <nerokyt2@gmail.com>
2022-07-15 12:24:34 +0800

    Create PersonalPageProfile.js

---
+++ src/features/personal/components/PersonalPageProfile.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const PersonalPageProfile = () => {
+    return (
+        <div className={"flex flex-col w-full"}>
+            <div className={"flex flex-col"}>
+                <div className={"h-48 w-full bg-indigo-50"}>
+                    Hehe
+                </div>
+            </div>
+        </div>
+    );
+}
