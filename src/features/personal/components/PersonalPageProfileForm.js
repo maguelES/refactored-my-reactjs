d80f18bccf490490d7f6c0317fc0a1b42eef47bd
Mohd Amsyar <nerokyt2@gmail.com>
2022-07-19 16:34:31 +0800

    Test tailwind form

--- src/features/personal/components/PersonalPageProfileForm.js
+++ src/features/personal/components/PersonalPageProfileForm.js
@@ -2,31 +2,65 @@ import React from "react";
 
 export const PersonalPageProfileForm = () => {
     return (
-        <form className={"grid grid-cols-1 gap-6 justify-items-start mx-5 my-5"}>
-            <label className="block">
-                <span className="text-gray-700">Full name</span>
-                <input type="text" className="
-                    mt-1
-                    block
-                    w-full
-                    rounded-md
-                    border-gray-300
-                    shadow-sm
-                    focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
-                  " placeholder=""/>
-            </label>
-            <label className="block">
-                <span className="text-gray-700">Full name</span>
-                <input type="text" className="
-                    mt-1
-                    block
-                    w-full
-                    rounded-md
-                    border-gray-300
-                    shadow-sm
-                    focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
-                  " placeholder=""/>
-            </label>
-        </form>
+        <div>
+            <form action="#" method="POST">
+                <div className="px-4 py-5 bg-white sm:p-6">
+                    <div className="grid grid-cols-6 gap-6">
+                        <div className="col-span-12">
+                            <label htmlFor="first-name" className="block text-left text-sm font-medium text-gray-700">First
+                                name</label>
+                            <input type="text" name="first-name" id="first-name" autoComplete="given-name"
+                                   className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+                        </div>
+
+                        <div className="col-span-6 sm:col-span-4">
+                            <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">Email
+                                address</label>
+                            <input type="text" name="email-address" id="email-address" autoComplete="email"
+                                   className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+                        </div>
+
+                        <div className="col-span-6 sm:col-span-3">
+                            <label htmlFor="country"
+                                   className="block text-sm font-medium text-gray-700">Country</label>
+                            <select id="country" name="country" autoComplete="country-name"
+                                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                <option>United States</option>
+                                <option>Canada</option>
+                                <option>Mexico</option>
+                            </select>
+                        </div>
+
+                        <div className="col-span-6">
+                            <label htmlFor="street-address" className="block text-sm font-medium text-gray-700">Street
+                                address</label>
+                            <input type="text" name="street-address" id="street-address"
+                                   autoComplete="street-address"
+                                   className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+                        </div>
+
+                        <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                            <label htmlFor="city" className="block text-sm font-medium text-gray-700">City</label>
+                            <input type="text" name="city" id="city" autoComplete="address-level2"
+                                   className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+                        </div>
+
+                        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                            <label htmlFor="region" className="block text-sm font-medium text-gray-700">State /
+                                Province</label>
+                            <input type="text" name="region" id="region" autoComplete="address-level1"
+                                   className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+                        </div>
+
+                        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                            <label htmlFor="postal-code" className="block text-sm font-medium text-gray-700">ZIP /
+                                Postal code</label>
+                            <input type="text" name="postal-code" id="postal-code" autoComplete="postal-code"
+                                   className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
     );
 }
