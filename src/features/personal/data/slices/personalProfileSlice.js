e113ad1ebad46b5612e2a0928c459d11c1ea1b4e
Mohd Amsyar <nerokyt2@gmail.com>
2022-09-05 12:06:09 +0800

    Prepare for form value set

--- src/features/personal/data/slices/personalProfileSlice.js
+++ src/features/personal/data/slices/personalProfileSlice.js
@@ -5,9 +5,16 @@ export const personalProfileSlice = createSlice({
     initialState: {
         isBusy: false,
     },
-    reducers: {}
+    reducers: {
+        profileFirstNameChange(state, action) {
+            console.log("Payload: ", action.payload.firstName)
+            state.firstName = action.payload.firstName;
+        }
+    }
 })
 
 export const selectPersonalProfileBusy = state => state.personalProfile.isBusy;
+
+export const {profileFirstNameChange} = personalProfileSlice.actions;
 
 export default personalProfileSlice.reducer;
